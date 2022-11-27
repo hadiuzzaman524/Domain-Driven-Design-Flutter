32c82caa0a4581398f6fdad25cd6534c93441c0f
hadi.heavytask <hadi@heavytask.com>
2022-11-27 22:44:34 +0600

    refactor: change variable name

--- lib/injection_configuration.dart
+++ lib/injection_configuration.dart
@@ -2,11 +2,11 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'injection_configuration.config.dart';
 
-final getIt = GetIt.instance;
+final injectable = GetIt.instance;
 
 @InjectableInit(
   initializerName: r'$initGetIt', // default
   preferRelativeImports: true, // default
   asExtension: false, // default
 )
-void configureDependencies() => $initGetIt(getIt);
+void configureDependencies() => $initGetIt(injectable);
